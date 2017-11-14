988a0e1c8f6ac6f222ac499702ca7aba217f7b64
billythekid <billy@the-kid.org>
2017-11-14 11:02:25 +0000

    Updated

--- public/index.php
+++ public/index.php
@@ -1,20 +1,28 @@
 <?php
 
 // Path to your craft/ folder
-$craftPath = '/home/eqdev/sites/god/craft';
-define('CRAFT_BASE_PATH', '../craft/');
-
+switch ($_SERVER['HTTP_HOST'])
+{
+    case 'jordan.dev':
+        $craftPath = '../craft';
+        define('CRAFT_BASE_PATH', '../craft/');
+        break;
+    default:
+        $craftPath = '/home/eqdev/sites/god/craft';
+        define('CRAFT_BASE_PATH', '../craft/');
+        break;
+}
 // Do not edit below this line
-$path = rtrim($craftPath, '/').'/app/index.php';
+$path = rtrim($craftPath, '/') . '/app/index.php';
 
 if (!is_file($path))
 {
-	if (function_exists('http_response_code'))
-	{
-		http_response_code(503);
-	}
+    if (function_exists('http_response_code'))
+    {
+        http_response_code(503);
+    }
 
-	exit('Could not find your craft/ folder. Please ensure that <strong><code>$craftPath</code></strong> is set correctly in '.__FILE__);
+    exit('Could not find your craft/ folder. Please ensure that <strong><code>$craftPath</code></strong> is set correctly in ' . __FILE__);
 }
 
 require_once $path;
